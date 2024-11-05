661a389cf0a3702379a0ba9a2acb286b4d1ce994
Oleksandr Burdun <oburdun@playtika.com>
2024-11-05 12:13:54 +0200

    add ability to not start bot

--- server.ts
+++ server.ts
@@ -3,7 +3,9 @@ import {DServer} from "./src/express.js";
 import {env} from "./config.js";
 
 const dbot: DBot = new DBot(env.botToken);
-dbot.Run()
+if (!env.botIgnoreStart) {
+    dbot.Run()
+}
 
 const server: DServer = new DServer(env.port, dbot);
 server.Run()
